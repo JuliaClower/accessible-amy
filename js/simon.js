032ba6c25c87f2c82ff0eeed2c3fde7102cd4fee
Julia Clower <juliaclower@Julias-MacBook-Air.local>
2019-10-03 10:49:16 -0400

    random number pushing to array

--- js/simon.js
+++ js/simon.js
@@ -5,30 +5,30 @@ console.log("hi amy")
 //click events for each button to show that shadow and then turn off
 const token1Button = document.querySelector('#token1')
 
-token1Button.addEventListener('click', function(){
+token1Button.addEventListener('click', function () {
     $('#token1').css('box-shadow', '0px 0px 83px 3px rgba(30,30,189,1)')
     setTimeout(turnOffShadow, 200)
 })
 const token2Button = document.querySelector('#token2')
 
-token2Button.addEventListener('click', function(){
+token2Button.addEventListener('click', function () {
     $('#token2').css('box-shadow', '0px 0px 83px 3px rgba(30,30,189,1)')
     setTimeout(turnOffShadow, 200)
 })
 const token3Button = document.querySelector('#token3')
 
-token3Button.addEventListener('click', function(){
+token3Button.addEventListener('click', function () {
     $('#token3').css('box-shadow', '0px 0px 83px 3px rgba(30,30,189,1)')
     setTimeout(turnOffShadow, 200)
 })
 const token4Button = document.querySelector('#token4')
 
-token4Button.addEventListener('click', function(){
+token4Button.addEventListener('click', function () {
     $('#token4').css('box-shadow', '0px 0px 83px 3px rgba(30,30,189,1)')
     setTimeout(turnOffShadow, 200)
 })
 
-const turnOffShadow = () =>{
+const turnOffShadow = () => {
     $('#token1').css('box-shadow', 'none')
     $('#token2').css('box-shadow', 'none')
     $('#token3').css('box-shadow', 'none')
@@ -38,7 +38,7 @@ const turnOffShadow = () =>{
 //Accessibility buttons below
 const monoButton = document.querySelector('.monochromatic')
 
-changeToMono = () =>{
+changeToMono = () => {
     removeImages()
     $('#token1').css("background-color", "black")
     $('#token2').css("background-color", "gray")
@@ -51,7 +51,7 @@ monoButton.addEventListener('click', changeToMono)
 
 const triDiButton = document.querySelector('.tri-di-chromatic')
 
-triDiButton.addEventListener('click', function(){
+triDiButton.addEventListener('click', function () {
     removeImages()
     $('#token1').css("background-color", "palevioletred")
     $('#token2').css("background-color", "palegreen")
@@ -61,7 +61,7 @@ triDiButton.addEventListener('click', function(){
 
 const physicalButton = document.querySelector('.physical')
 
-physicalButton.addEventListener('click', function(){
+physicalButton.addEventListener('click', function () {
     $('#token1').css("background-color", "purple")
     $('#token2').css("background-color", "pink")
     $('#token3').css("background-color", "salmon")
@@ -72,7 +72,7 @@ physicalButton.addEventListener('click', function(){
     $('#token4').html('<img id="number1" src="css/pictures/number4.png" />')
 })
 
-const removeImages = () =>{
+const removeImages = () => {
     $('#token1').html('')
     $('#token2').html('')
     $('#token3').html('')
@@ -80,31 +80,34 @@ const removeImages = () =>{
 }
 
 //game logic below
-let getRandom = function(){
+let getRandom = function() {
     const min = 1
     const max = 5
     let random = Math.floor(Math.random() * (+max - +min)) + +min
     console.log(random)
-    if(random === 1)
-    console.log('button1')
-    if(random === 2)
-    console.log('button2')
-    if(random === 3)
-    console.log('button3')
-    if(random === 4)
-    console.log('button4')
+    if (random === 1) {
+        console.log('button1')
+    }
+    if (random === 2) {
+        console.log('button2')
+    }
+    if (random === 3) {
+        console.log('button3')
+    }
+    if (random === 4) {
+        console.log('button4')
+    }
+    return random
 }
 
-// let genColor = function(){
-//     if(random = 1)
-//     console.log('button1')
-//     if(random = 2)
-//     console.log('button2')
-//     if(random = 3)
-//     console.log('button3')
-//     if(random = 4)
-//     console.log('button4')
-// }
+let randomArray = function () {
+    console.log('getRandom', getRandom)
+    let numArray = []
+    for (i = 0; i < 6; i++) {
+        numArray.push(getRandom())
+        console.log(numArray)
+    }
+    return numArray;
+}
 
-getRandom()
-
+randomArray()
