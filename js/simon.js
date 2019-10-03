3663d4a2fb800d84885000f52d17db25a99862cc
Julia Clower <juliaclower@Julias-MacBook-Air.local>
2019-10-03 09:37:38 -0400

    box shadow add and end time functioning

--- js/simon.js
+++ js/simon.js
@@ -1,31 +1,56 @@
 console.log("hi amy")
 
+//Token Click Events (sound and highlight)
+const token1Button = document.querySelector('#token1')
+
+token1Button.addEventListener('click', function(){
+    $('#token1').css('box-shadow', '0px 0px 83px 3px rgba(30,30,189,1)')
+    setTimeout(turnOffShadow, 200)
+})
+const token2Button = document.querySelector('#token2')
+
+token2Button.addEventListener('click', function(){
+    $('#token2').css('box-shadow', '0px 0px 83px 3px rgba(30,30,189,1)')
+    setTimeout(turnOffShadow, 200)
+})
+const token3Button = document.querySelector('#token3')
+
+token3Button.addEventListener('click', function(){
+    $('#token3').css('box-shadow', '0px 0px 83px 3px rgba(30,30,189,1)')
+    setTimeout(turnOffShadow, 200)
+})
+const token4Button = document.querySelector('#token4')
+
+token4Button.addEventListener('click', function(){
+    $('#token4').css('box-shadow', '0px 0px 83px 3px rgba(30,30,189,1)')
+    setTimeout(turnOffShadow, 200)
+})
+
+const turnOffShadow = () =>{
+    $('#token1').css('box-shadow', 'none')
+    $('#token2').css('box-shadow', 'none')
+    $('#token3').css('box-shadow', 'none')
+    $('#token4').css('box-shadow', 'none')
+}
+
+//Accessibility buttons below
 const monoButton = document.querySelector('.monochromatic')
-var divClone = $("#game-wrapper").clone();
-
-// remvoeImages = () =>{
-
-// }
-
-// var image_x = document.getElementById('image_X');
-// image_x.parentNode.removeChild(image_x);
 
 changeToMono = () =>{
+    removeImages()
     $('#token1').css("background-color", "black")
     $('#token2').css("background-color", "gray")
     $('#token3').css("background-color", "darkblue")
     $('#token4').css("background-color", "darkgray")
     console.log("banana")
 }
-monoButton.addEventListener('click', function(){
-    $("#game-wrapper").replaceWith(divClone);
-    setTimeout(changeToMono, 50)
-})
+
+monoButton.addEventListener('click', changeToMono)
 
 const triDiButton = document.querySelector('.tri-di-chromatic')
 
 triDiButton.addEventListener('click', function(){
-    location.reload(true)
+    removeImages()
     $('#token1').css("background-color", "palevioletred")
     $('#token2').css("background-color", "palegreen")
     $('#token3').css("background-color", "lightblue")
@@ -35,10 +60,22 @@ triDiButton.addEventListener('click', function(){
 const physicalButton = document.querySelector('.physical')
 
 physicalButton.addEventListener('click', function(){
-    $('#token1').prepend('<img id="number1" src="css/pictures/number1.png" />')
-    $('#token2').prepend('<img id="number1" src="css/pictures/number2.png" />')
-    $('#token3').prepend('<img id="number1" src="css/pictures/number3.png" />')
-    $('#token4').prepend('<img id="number1" src="css/pictures/number4.png" />')
+    $('#token1').css("background-color", "purple")
+    $('#token2').css("background-color", "pink")
+    $('#token3').css("background-color", "salmon")
+    $('#token4').css("background-color", "cyan")
+    $('#token1').html('<img id="number1" src="css/pictures/number1.png" />')
+    $('#token2').html('<img id="number1" src="css/pictures/number2.png" />')
+    $('#token3').html('<img id="number1" src="css/pictures/number3.png" />')
+    $('#token4').html('<img id="number1" src="css/pictures/number4.png" />')
 })
+
+const removeImages = () =>{
+    $('#token1').html('')
+    $('#token2').html('')
+    $('#token3').html('')
+    $('#token4').html('')
+}
+
 
 
